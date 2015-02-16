177357d674f37fbf8b1810b7c2c2c13b7f967abb
shiya <siya1991925@gmail.com>
2015-02-16 16:53:28 +0000

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Demo_app2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
